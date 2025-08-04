72236a6711d077274e2fef3c36eb9368f9964c11
Bassirou Tall <bassirou.tall@epitech.eu>
2025-08-04 16:40:56 +0200

    fix: add scriptpath to indicate in which file servers are associated to

--- ServerDetectionApp.cs
+++ ServerDetectionApp.cs
@@ -21,17 +21,23 @@ namespace ServerDetecionApp
 
         private string DetectedServer(string serverType)
         {
+            string scriptpath = string.Empty;
             switch (serverType)
             {
                 case "Active Directory":
+                    /*scriptpath*/
                     return "Active Directory Server is running on this device.";
                 case "mssql":
+                    /*scriptpath*/
                     return "mssql Server is running on this device.";
                 case "windows server 2019":
+                    /*scriptpath*/
                     return "windows server 2019 is running on this device";
                 case "windows server 2021":
+                    /*scriptpath*/
                     return "windows server 2021 is running on this device";
                 default:
+                    scriptpath = "C:\\Users\\Public\\Server-scripting\\smbv1checker.ps1";
                     return "Basic server is running on this device.";
             }
         }
