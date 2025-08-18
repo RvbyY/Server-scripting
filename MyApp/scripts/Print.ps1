a4fc2b3e94b023f8a380bff770f6bf6dfb93808c
Bassirou Tall <bassirou.tall@epitech.eu>
2025-08-18 16:59:32 +0200

    feat: create users admin list in print server

--- MyApp/scripts/Print.ps1
+++ MyApp/scripts/Print.ps1
@@ -1,1 +1,11 @@
 .\smbv1checker.ps1
+
+function ListUsers
+{
+    $admins = Get-localGroupMember -Group "Administrators" | Select-Object Name
+
+    "=== Admins Users ===" | Out-File -FilePath ".\info.txt" -Append -Encoding utf8
+    foreach ($admin in $admins) {
+        "$($admin)" | Out-File -FilePath ".\info.txt" -Append -Encoding utf8
+    }
+}
