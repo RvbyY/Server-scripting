3282ac52e5a0ea69c7385156ebeef8704666ce51
Bassirou Tall <bassirou.tall@epitech.eu>
2025-08-25 09:35:54 +0200

    desiabled: User acoount credentials AD function

--- MyApp/bin/Debug/net9.0-windows/scripts/ActiveDirectory.ps1
+++ MyApp/bin/Debug/net9.0-windows/scripts/ActiveDirectory.ps1
@@ -4,24 +4,24 @@
 .DESCRIPTION
 Test Local User Account Credentials
 #>
-function TestUserCredentials
-{
-    Write-Verbose "Prompting for password"
-    $pswd = Read-Host "Type password -- VERIFY BEFORE CLICKING RETURN!!!"  -assecurestring
-    $decodedpswd = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pswd))
+# function TestUserCredentials
+# {
+#     Write-Verbose "Prompting for password"
+#     $pswd = Read-Host "Type password -- VERIFY BEFORE CLICKING RETURN!!!"  -assecurestring
+#     $decodedpswd = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pswd))
 
-    Foreach ($computer in $computers) {
-        $username = "variable with local admin user"
-        Add-Type -AssemblyName System.DirectoryServices.AccountManagement
-        $obj = New-Object System.DirectoryServices.AccountManagement.PrincipalContext('machine', $computer)
-        if ($obj.ValidateCredentials($username, $decodedpswd) -eq $True) {
-            Write-Host "The password of UserName $($username) in Computer $($computer) it is correct" -BackgroundColor Green
-        }
-        else {
-            Write-Host "The password of UserName $($username) in Computer $($computer) does not is correct" -BackgroundColor Red
-        }
-    }
-}
+#     Foreach ($computer in $computers) {
+#         $username = "variable with local admin user"
+#         Add-Type -AssemblyName System.DirectoryServices.AccountManagement
+#         $obj = New-Object System.DirectoryServices.AccountManagement.PrincipalContext('machine', $computer)
+#         if ($obj.ValidateCredentials($username, $decodedpswd) -eq $True) {
+#             Write-Host "The password of UserName $($username) in Computer $($computer) it is correct" -BackgroundColor Green
+#         }
+#         else {
+#             Write-Host "The password of UserName $($username) in Computer $($computer) does not is correct" -BackgroundColor Red
+#         }
+#     }
+# }
 
 <#
 .DESCRIPTION
