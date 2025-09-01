b1a54929d344dafb657e8a59d4560cfb9d45f1e3
Bassirou Tall <bassirou.tall@epitech.eu>
2025-09-01 15:14:08 +0200

    feat: fix crashs issues in Active Directory (disabled users credentials function ) and in msSQL (disabled last password change, localusersql and checkpwdcomplexity functions)

--- MyApp/bin/Debug/net9.0-windows/scripts/MsSql.ps1
+++ MyApp/bin/Debug/net9.0-windows/scripts/MsSql.ps1
@@ -7,11 +7,15 @@ function ListSQLServices
     $Services = Get-Service | Where-Object DisplayName -Like "SQL*" -ErrorAction silentlyContinue
 
     "=== SQL Server Services ===" | Out-File -FilePath ".\info.txt" -Append -Encoding utf8
-    if (!$Services) {
-        "No SQL server services found" | Out-File -Filepath ".\info.txt" -Append -Encoding utf8
-        return
+    try {
+        if (!$Services) {
+            "No SQL server services found" | Out-File -Filepath ".\info.txt" -Append -Encoding utf8
+            return
+        }
+        "$($Services)" | Out-File -FilePath ".\info.txt" -Append -Encoding utf8
+    } catch {
+        "Error in ListSQlServices: $($_.Exception.Message)" | Out-File -Filepath ".\info.txt" -Append -Encoding utf8
     }
-    "$($Services)" | Out-File -FilePath ".\info.txt" -Append -Encoding utf8
 }
 
 <#
@@ -20,21 +24,29 @@ List Admin Users
 #>
 function ListAdminUsers
 {
-    $serverInstance = Get-Service | Where-Object { $_.Name -like 'MSSQL*' } | Select-Object DisplayName, Status -ErrorAction silentlyContinue
-    $query = "SELECT name FROM sys.server_principals WHERE is_fixed_role = 1 AND name = 'sysadmin'"
+    $serverInstance = Get-SQLInstance -ErrorAction silentlyContinue
+    $query = @"
+    SELECT name, type_desc, is_disabled
+    FROM sys.server_principals
+    WHERE IS_SRVROLEMEMBER('sysadmin', name) = 1
+"@
     $adminUsers = Invoke-Sqlcmd -ServerInstance $serverInstance -Query $query -ErrorAction silentlyContinue
 
     "=== Admin Users ===" | Out-File -Filepath ".\info.txt" -Append -Encoding utf8
     if (!$adminUsers) {
         "No admin users found" | Out-file -FilePath -Append -Encodding utf8
         return
+    } else {
+        foreach ($user in $adminUsers) {
+            "$($user.name) - $($user.type_desc) - Disabled: $($user.is_disabled)" | Out-File -FilePath ".\info.txt" -Append -Encoding utf8
+        }
     }
-    "$($adminUsers)" | Out-File -FilePath ".\info.txt" -Append -Encoding utf8
 }
 
 <#
 .DESCRIPTION
-Display and list disabled admin Users#>
+Display and list disabled admin Users
+#>
 function DisabledUsers
 {
     $serverInstance = Get-Service | Where-Object { $_.Name -like 'MSSQL*' } | Select-Object DisplayName, Status -ErrorAction silentlyContinue
@@ -48,48 +60,110 @@ function DisabledUsers
     }
     "$($disabledUsers)" | Out-File -FilePath ".\info.txt" -Append -Encoding utf8
 }
+# function DisabledUsers
+# {
+#     try {
+#         $instances = Get-SQLInstance
+#         if (!$instances) {
+#             return
+#         }
+#         "=== Disabled Admin Users ===" | Out-File -Filepath ".\info.txt" -Append -Encoding utf8
+#         foreach ($instance in $instances) {
+#             if ($instance -eq "DEFAULT") {
+#                 $serverInstance = "localhost"
+#             } else {
+#                 $serverInstance = "localhost\$instance"
+#             }
+#             $query = @"
+# SELECT name, type_desc
+# FROM sys.server_principals
+# WHERE IS_SRVROLEMEMBER('sysadmin', name) = 1 AND is_disabled = 1
+# "@
+#             $disabledUsers = Invoke-Sqlcmd -ServerInstance $serverInstance -Query $query -ErrorAction Stop
+#             if ($disabledUsers) {
+#                 "Instance: $serverInstance" | Out-File -FilePath ".\info.txt" -Append -Encoding utf8
+#                 foreach ($user in $disabledUsers) {
+#                     "$($user.name) - $($user.type_desc)" | Out-File -FilePath ".\info.txt" -Append -Encoding utf8
+#                 }
+#             } else {
+#                     "No disabled admin users found in $serverInstance" | Out-File -Filepath ".\info.txt" -Append -Encoding utf8
+#             }
+#         }
+#     } catch {
+#         "Error in DisabledUsers: $($_.Exception.Message)" | Out-File -FilePath ".\info.txt" -Append -Encoding utf8
+#     }
+# }
 
 <#
 .DESCRIPTION
 See last Users login
 #>
+# function LastUsersLog
+# {
+#     $serverInstance = Get-Service | Where-Object { $_.Name -like 'MSSQL*' } | Select-Object DisplayName, Status -ErrorAction silentlyContinue
+#     $query = "SELECT p.name, p.type_desc, s.last_login
+#         FROM sys.server_principals p
+#         LEFT JOIN sys.syslogins s ON p.sid = s.sid
+#         WHERE p.is_fixed_role = 1 AND p.name = 'sysadmin'"
+#     $adminUserLog = Invoke-Sqlcmd -ServerInstance $serverInstance -Query $query -ErrorAction silentlyContinue
+
+#     "=== Admin Last Login ===" | Out-File -FilePath ".\info.txt" -Append -Encoding utf8
+#     if (!$adminUserLog) {
+#         "No last login admin log found" | Out-file -Append -Encoding utf8
+#         return
+#     }
+#     "$($adminUserLog)" | Out-File -FilePath ".\info.txt" -Append -Encoding utf8
+# }
 function LastUsersLog
 {
-    $serverInstance = Get-Service | Where-Object { $_.Name -like 'MSSQL*' } | Select-Object DisplayName, Status -ErrorAction silentlyContinue
-    $query = "SELECT p.name, p.type_desc, s.last_login
-        FROM sys.server_principals p
-        LEFT JOIN sys.syslogins s ON p.sid = s.sid
-        WHERE p.is_fixed_role = 1 AND p.name = 'sysadmin'"
-    $adminUserLog = Invoke-Sqlcmd -ServerInstance $serverInstance -Query $query -ErrorAction silentlyContinue
-
     "=== Admin Last Login ===" | Out-File -FilePath ".\info.txt" -Append -Encoding utf8
-    if (!$adminUserLog) {
-        "No last login admin log found" | Out-file -Append -Encoding utf8
-        return
+    try {
+        $instances = Get-SQLInstance
+        if (!$instances) { return }
+        foreach ($instance in $instances) {
+            $serverInstance = if ($instance -eq "DEFAULT") { "localhost" } else { "localhost\$instance" }
+            $query = @"
+SELECT p.name, p.type_desc, MAX(s.last_successful_logon) as last_login
+FROM sys.server_principals p
+LEFT JOIN sys.dm_exec_sessions s ON p.sid = s.security_id
+WHERE IS_SRVROLEMEMBER('sysadmin', p.name) = 1
+GROUP BY p.name, p.type_desc
+"@
+            $adminUserLog = Invoke-Sqlcmd -ServerInstance $serverInstance -Query $query -ErrorAction Stop
+            if ($adminUserLog) {
+                "Instance: $serverInstance" | Out-File -FilePath ".\info.txt" -Append -Encoding utf8
+                foreach ($log in $adminUserLog) {
+                    "$($log.name) - Last Login: $($log.last_login)" | Out-File -FilePath ".\info.txt" -Append -Encoding utf8
+                }
+            } else {
+                "No login information found for $serverInstance" | Out-File -FilePath ".\info.txt" -Append -Encoding utf8
+            }
+        }
+    } catch {
+        "Error in LastUsersLog: $($_.Exception.Message)" | Out-File -FilePath ".\info.txt" -Append -Encoding utf8
     }
-    "$($adminUserLog)" | Out-File -FilePath ".\info.txt" -Append -Encoding utf8
 }
 
 <#
 .DESCRIPTION
 Display last password change
 #>
-function LastPwdChange
-{
-    $serverInstance = Get-Service | Where-Object { $_.Name -like 'MSSQL*' } | Select-Object DisplayName, Status -ErrorAction silentlyContinue
-    $query = "SELECT p.name, p.type_desc, s.password_changed
-        FROM sys.server_principals p
-        LEFT JOIN sys.sql_logins s ON p.sid = s.sid
-        WHERE p.is_fixed_role = 1 AND p.name = 'sysadmin'"
-    $adminLastPwd = Invoke-Sqlcmd -ServerInstance $serverInstance -Query $query -ErrorAction silentlyContinue
+# function LastPwdChange
+# {
+#     $serverInstance = Get-Service | Where-Object { $_.Name -like 'MSSQL*' } | Select-Object DisplayName, Status -ErrorAction silentlyContinue
+#     $query = "SELECT p.name, p.type_desc, s.password_changed
+#         FROM sys.server_principals p
+#         LEFT JOIN sys.sql_logins s ON p.sid = s.sid
+#         WHERE p.is_fixed_role = 1 AND p.name = 'sysadmin'"
+#     $adminLastPwd = Invoke-Sqlcmd -ServerInstance $serverInstance -Query $query -ErrorAction silentlyContinue
 
-    "=== Admin Last Password change ===" | Out-File -Filepath ".\info.txt" -Append -Encoding utf8
-    if (!$adminLastPwd) {
-        "No admin last password change log found" | Out-File -Filepath ".\info.txt" -Append -Encoding utf8
-        return
-    }
-    "$($adminLastPwd)" | Out-File -FilePath ".\info.txt" -Append -Encoding utf8
-}
+#     "=== Admin Last Password change ===" | Out-File -Filepath ".\info.txt" -Append -Encoding utf8
+#     if (!$adminLastPwd) {
+#         "No admin last password change log found" | Out-File -Filepath ".\info.txt" -Append -Encoding utf8
+#         return
+#     }
+#     "$($adminLastPwd)" | Out-File -FilePath ".\info.txt" -Append -Encoding utf8
+# }
 
 <#
 .DESCRIPTION
@@ -113,17 +187,20 @@ List Disabled Spooler
 #>
 function IsSpoolerEnable
 {
-    $spoolers = Get-Service -Name Spooler | Select-Object DisplayName, Status -ErrorAction silentlyContinue
-
-    "=== Disabled Spoolers ===" | Out-File -Filepath ".\info.txt" -Append -Encoding utf8
-    if (!$spoolers) {
-        "No disabled spoolers found" | Out-File -Filepath ".\info.txt" -Append -Encoding utf8
-        return
-    }
-    foreach ($spooler in $spoolers) {
-        if ($spooler.StartType -eq 'Disabled') {
-            "$($spooler.Name): $($spooler.Status)" | Out-File -FilePath ".\info.txt" -Append -Encoding utf8
+    try {
+        $spoolers = Get-Service -Name Spooler -ErrorAction silentlyContinue
+        "=== Disabled Spoolers ===" | Out-File -Filepath ".\info.txt" -Append -Encoding utf8
+        if (!$spoolers) {
+            "No disabled spoolers found" | Out-File -Filepath ".\info.txt" -Append -Encoding utf8
+            return
         }
+        foreach ($spooler in $spoolers) {
+            if ($spooler.StartType -eq 'Disabled') {
+                "$($spooler.StartType): $($spooler.Status)" | Out-File -FilePath ".\info.txt" -Append -Encoding utf8
+            }
+        }
+    } catch {
+        "Error in IsSpoolerEnable: $($_.Exception.Message)" | Out-File -FilePath ".\info.txt" -Append -Encoding utf8
     }
 }
 
@@ -131,19 +208,19 @@ function IsSpoolerEnable
 .DESCRIPTION
 Local User SQL
 #>
-function LocalUserSql
-{
-    $serverInstance = Get-Service | Where-Object { $_.Name -like 'MSSQL*' } | Select-Object DisplayName, Status -ErrorAction silentlyContinue
-    $query = "SELECT name, type_desc FROM sys.database_principals WHERE type IN ('S', 'U') AND sid 0x0"
-    $localUsers = Invoke-Sqlcmd -ServerInstance $serverInstance -Database 'master' -Query $query -ErrorAction silentlyContinue
+# function LocalUserSql
+# {
+#     $serverInstance = Get-Service | Where-Object { $_.Name -like 'MSSQL*' } | Select-Object DisplayName, Status -ErrorAction silentlyContinue
+#     $query = "SELECT name, type_desc FROM sys.database_principals WHERE type IN ('S', 'U') AND sid 0x0"
+#     $localUsers = Invoke-Sqlcmd -ServerInstance $serverInstance -Database 'master' -Query $query -ErrorAction silentlyContinue
 
-    "=== Local Users SQL ===" | Out-File -Filepath ".\info.txt" -Append -Encoding utf8
-    if (!$localUsers) {
-        "No SQL local users found" | Out-File -Filepath ".\info.txt" -Append -Encoding utf_
-        return
-    }
-    "$($localusers)" | Out-File -FilePath ".\info.txt" -Append -Enconding utf8
-}
+#     "=== Local Users SQL ===" | Out-File -Filepath ".\info.txt" -Append -Encoding utf8
+#     if (!$localUsers) {
+#         "No SQL local users found" | Out-File -Filepath ".\info.txt" -Append -Encoding utf_
+#         return
+#     }
+#     "$($localusers)" | Out-File -FilePath ".\info.txt" -Append -Enconding utf8
+# }
 
 <#
 .DESCRIPTION
@@ -166,19 +243,19 @@ function checkWebDAV
 .DESCRIPTION
 Check Password Complexity of each admin user
 #>
-function CheckPwdComplexity
-{
-    $admins = Get-LocalUser | Where-Object { $_.Name -eq "Administrator"} -ErrorAction silentlyContinue
+# function CheckPwdComplexity
+# {
+#     $admins = Get-LocalUser | Where-Object { $_.Name -eq "Administrator"} -ErrorAction silentlyContinue
 
-    "=== Admins Paswword Complexity ===" | Out-File -Filepath ".\info.txt" -Append -Encoding utf8
-    if (!$admins) {
-        "No complexity log found" | Out-file -Filepath ".\info.txt" -Append -Encoding utf8
-        return
-    }
-    foreach ($admin in $admins) {
-        "$($admin.Name): $($admin.PasswordComplexity)" | Out-File -Filepath ".\info.txt" -Append -Encoding utf8
-    }
-}
+#     "=== Admins Paswword Complexity ===" | Out-File -Filepath ".\info.txt" -Append -Encoding utf8
+#     if (!$admins) {
+#         "No complexity log found" | Out-file -Filepath ".\info.txt" -Append -Encoding utf8
+#         return
+#     }
+#     foreach ($admin in $admins) {
+#         "$($admin.Name): $($admin.PasswordComplexity)" | Out-File -Filepath ".\info.txt" -Append -Encoding utf8
+#     }
+# }
 
 <#
 .DESCRIPTION
@@ -186,17 +263,15 @@ MSSql windows server main function#>
 function SQLmain
 {
     ListSQLServices
-    IsSpoolerEnable
-    LastPwdChange
-    LastUsersLog
-    DisabledUsers
     ListAdminUsers
-    ListSQLServices
-    checkWebDAV
-    LocalUserSql
-    IsSpoolerEnable
+    DisabledUsers
+    LastUsersLog
+    #LastPwdChange
     ListServerService
-    CheckPwdComplexity
+    IsSpoolerEnable
+    #LocalUserSql
+    checkWebDAV
+    #CheckPwdComplexity
 }
 
 SQLmain
